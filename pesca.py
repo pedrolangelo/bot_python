b04589cc376731eb851e5b942e0ccc1bfb47b855
Lucas Moura Alcantara <lucasmoura404@gmail.com>
2023-10-01 20:26:05 -0300

    pesquei 1

--- pesca.py
+++ pesca.py
@@ -2,7 +2,7 @@ import pyautogui
 from apis import pegar_peixe, checar
 
 def pescar():
-    pesquei     = pyautogui.locateOnScreen('imagens/pesquei.png', grayscale=True, confidence=0.4)
+    pesquei     = pyautogui.locateOnScreen('imagens/pesquei1.png', grayscale=True, confidence=0.4)
     checagem    = pyautogui.locateOnScreen('imagens/checagem.png', grayscale=True, confidence=0.4)
 
     if pesquei is not None:
