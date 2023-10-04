09ec17d76249de3c3d040498d617872fb0662474
Lucas Moura Alcantara <lucasmoura404@gmail.com>
2023-10-04 02:11:54 -0300

    update

--- pesca.py
+++ pesca.py
@@ -2,7 +2,7 @@ import pyautogui
 from apis import pegar_peixe, checar, comer_beber
 
 def pescar():
-    pesquei     = pyautogui.locateOnScreen('imagens/pesquei1.png', grayscale=True, confidence=0.4)
+    pesquei     = pyautogui.locateOnScreen('imagens/pesquei.png', grayscale=True, confidence=0.4)
     checagem    = pyautogui.locateOnScreen('imagens/checagem.png', grayscale=True, confidence=0.4)
     comida      = pyautogui.locateOnScreen('imagens/comida_vermelha.png', grayscale=True, confidence=0.4)
     agua        = pyautogui.locateOnScreen('imagens/agua_vermelha.png', grayscale=True, confidence=0.4)
